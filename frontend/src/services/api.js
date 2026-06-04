07a9d5e25bb5a7333e8b82c4d2b2bf5981da2bdc
susiloardi <susiloardiansyah0@gmail.com>
2026-06-04 19:57:01 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://full-stack-inky-seven.vercel.app/'
+  baseURL: 'https://full-stack-inky-seven.vercel.app/api'
 });
 
 API.interceptors.request.use((config) => {
